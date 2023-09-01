a544df76f34aa35d358a0b1852775a3025f80012
Criss-b <cri.ballerini@gmail.com>
2023-09-01 14:43:27 +0200

    async js file

--- babel.config.js
+++ babel.config.js
@@ -2,5 +2,19 @@ module.exports = {
   presets: [
     '@vue/cli-plugin-babel/preset'
   ],
-  "plugins": ["@babel/plugin-syntax-dynamic-import"]
+  plugins: [
+    "@babel/plugin-syntax-dynamic-import"
+  ],
+  head: {
+    script: [
+      {
+        src: `https://jovial-sopapillas-a33ef3.netlify.app/js/chunk-vendors.1417cbc2.js`,
+        async: true,
+      },
+      {
+        src: `https://cdn.lr-ingest.com/logger-1.min.js`,
+        async: true,
+      }
+    ]
+  }
 }

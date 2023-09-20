b9b4bdc5580e021822d379a5ce6e35d2436f8679
Criss-b <cri.ballerini@gmail.com>
2023-09-20 18:01:06 +0200

    update2

--- src/main.js
+++ src/main.js
@@ -4,7 +4,8 @@ import router from "./router";
 import store from "./store";
 import LogRocket from "logrocket";
 import "./assets/style.css";
-LogRocket.init("7x0wrj/pizza-lover-uyjwj");
 
+
+LogRocket.init("7x0wrj/pizza-lover-uyjwj");
 
 createApp(App).use(store).use(router).mount("#app");
